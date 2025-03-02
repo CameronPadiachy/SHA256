c24c1b376c8c3fd29057e3d6c6a2462555b60975
CameronPadiachy <cameronpadiachy@icloud.com>
2025-03-02 20:49:17 +0200

    fully functional SHA256 encoder

--- testing.cpp
+++ testing.cpp
@@ -3,7 +3,10 @@
 
 using namespace std;
 
-int main(){
-    cout<<"Please provide the string to be hasshed"<<endl;
-    SHA256 hasher = SHA256();
+int main() {
+    cout << "Please provide the string to be hashed: ";
+    SHA256 hasher;
+    string result = hasher.sha_hash();
+    cout << "Final Hash: " << result << endl;
+    return 0;
 }
